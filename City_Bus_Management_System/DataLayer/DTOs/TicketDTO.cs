5744fc8eb6228a3779465ec67321c1e6cf0b05d3
Yusef <youssefmohmed430@gmail.com>
2025-09-28 14:18:46 +0300

    Refactor Ticket model and service for new properties
    - Replaced `NumberOfStations` with `MinStations` in the `Ticket` class.
    - Added `IsDeleted` property to `Ticket` and updated the database schema.
    - Introduced `TicketDTO` class with relevant properties.
    - Created migration `IniteV7` to handle database changes.
    - Updated `ITicketService` interface and `TicketService` implementation to reflect property name changes.

--- City_Bus_Management_System/DataLayer/DTOs/TicketDTO.cs
+++ City_Bus_Management_System/DataLayer/DTOs/TicketDTO.cs
@@ -1,12 +1,10 @@
-﻿namespace City_Bus_Management_System.DataLayer.Entities
+﻿namespace City_Bus_Management_System.DataLayer.DTOs
 {
-    public class Ticket
+    public class TicketDTO
     {
         public int Id { get; set; }
         public required int MinStations { get; set; }
         public required string BusType { get; set; }
         public required double Price { get; set; } // depends on NumberOfStations and BusType
-        public bool IsDeleted { get; set; } = false;
-        public List<Booking>? BookIngs { get; set; }
     }
 }
---
+++ City_Bus_Management_System/Migrations/20250928111727_IniteV7.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace City_Bus_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class IniteV7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NumberOfStations",
+                table: "Tickets",
+                newName: "MinStations");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Tickets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Tickets");
+
+            migrationBuilder.RenameColumn(
+                name: "MinStations",
+                table: "Tickets",
+                newName: "NumberOfStations");
+        }
+    }
+}
